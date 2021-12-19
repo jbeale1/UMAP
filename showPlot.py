f519c50a7e144cafe02efd09704a8ba79c3a1c24
John Beale <jbeale1@users.noreply.github.com>
2021-12-18 16:37:44 -0800

    change from file input to dataframe as param

--- showPlot.py
+++ showPlot.py
@@ -6,23 +6,24 @@ import os  # loop over images in directory
 import matplotlib.pyplot as plt
 import skimage.io   # to read & save images
 import numpy as np  # for np.zeros()
-from skimage import exposure  # adaptive hist. equalization
+# from skimage import exposure  # adaptive hist. equalization
 from skimage.transform import resize
 from skimage.util import img_as_ubyte
-from skimage.color import rgb2gray
-import pandas as pd    # pd.read_csv()
+# from skimage.color import rgb2gray
+# import pandas as pd    # pd.read_csv()
 # --------------------------------------------------------------
 
 xsize = 300  # image size, pixels across
 ysize = 115  # image size, pixels down
 
-
+"""
 def inorm(img):
     target_dim = (ysize, xsize)  # convert image to this size bitmap
     gray = rgb2gray(img)  # convert RGB image to greyscale
     img_eq = exposure.equalize_adapthist(gray, clip_limit=0.03)
     img2 = resize(img_eq, target_dim, anti_aliasing=True)
     return(img_as_ubyte(img2))
+"""
 
 
 def jresize(img):
@@ -30,9 +31,9 @@ def jresize(img):
     return(img_as_ubyte(img2))
 
 
-def p20():
+def p20(df):
     path = "C:\\Users\\beale\\Documents\\YOLO\\out\\car"  # read images here
-    fin = "Select20-carsB.csv"
+    # fin = "Select20-carsB.csv"
 
     xmul = 4   # how many images along this axis
     ymul = 5
@@ -42,7 +43,7 @@ def p20():
     xp = 0  # position of sub-image on canvas
     yp = 0  # position of sub-image on canvas
 
-    df = pd.read_csv(fin)  # load dataframe with image filenames
+    # df = pd.read_csv(fin)  # load dataframe with image filenames
 
     for i in range(xmul * ymul):
         iname = df.loc[i, 'fname']
@@ -65,9 +66,6 @@ if __name__ == '__main__':
 
 
 """
-# import showPlot
-# showPlot.p20()
-
 fname_out = fin + ".png"
 skimage.io.imsave(fname_out, canvas)  # save file
 """
